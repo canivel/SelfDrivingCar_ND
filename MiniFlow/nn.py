58af8dc7f5cf900b909c0fc619652f20bc41b35f
Danilo <d.canivel@gmail.com>
2017-06-08 13:50:57 -0400

    first commit

---
+++ MiniFlow/nn.py
@@ -0,0 +1,122 @@
+"""
+This script builds and runs a graph with miniflow.
+
+There is no need to change anything to solve this quiz!
+
+However, feel free to play with the network! Can you also
+build a network that solves the equation below?
+
+(x + y) + y
+"""
+
+from MiniFlow.miniflow import *
+import numpy as np
+
+
+x, y = Input(), Input()
+
+f = Add(x, y)
+
+feed_dict = {x: 10, y: 5}
+
+sorted_nodes = topological_sort(feed_dict)
+output = forward_pass(f, sorted_nodes)
+
+# NOTE: because topological_sort set the values for the `Input` nodes we could also access
+# the value for x with x.value (same goes for y).
+print("{} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], output))
+
+
+#SECOND NN
+x, y, z = Input(), Input(), Input()
+
+f = Add(x, y, z)
+
+feed_dict = {x: 4, y: 5, z: 10}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(f, graph)
+
+# should output 19
+print("{} + {} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[z], output))
+
+#THIRD NN - LINEAR
+
+"""
+NOTE: Here we're using an Input node for more than a scalar.
+In the case of weights and inputs the value of the Input node is
+actually a python list!
+
+In general, there's no restriction on the values that can be passed to an Input node.
+"""
+
+inputs, weights, bias = Input(), Input(), Input()
+
+f = Linear(inputs, weights, bias)
+
+feed_dict = {
+    inputs: [6, 14, 3],
+    weights: [0.5, 0.25, 1.4],
+    bias: 2
+}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(f, graph)
+
+print(output) # should be 12.7 with this example
+
+
+"""
+This network feeds the output of a linear transform
+to the sigmoid function.
+
+Finish implementing the Sigmoid class in miniflow.py!
+
+Feel free to play around with this network, too!
+"""
+
+X, W, b = Input(), Input(), Input()
+
+f = Linear(X, W, b)
+g = Sigmoid(f)
+
+X_ = np.array([[-1., -2.], [-1, -2]])
+W_ = np.array([[2., -3], [2., -3]])
+b_ = np.array([-3., -5])
+
+feed_dict = {X: X_, W: W_, b: b_}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(g, graph)
+
+"""
+Output should be:
+[[  1.23394576e-04   9.82013790e-01]
+ [  1.23394576e-04   9.82013790e-01]]
+"""
+print(output)
+
+
+"""
+Test your MSE method with this script!
+
+No changes necessary, but feel free to play
+with this script to test your network.
+"""
+y, a = Input(), Input()
+cost = MSE(y, a)
+
+y_ = np.array([1, 2, 3])
+a_ = np.array([4.5, 5, 10])
+
+feed_dict = {y: y_, a: a_}
+graph = topological_sort(feed_dict)
+# forward pass
+forward_pass_graph(graph)
+
+"""
+Expected output
+
+23.4166666667
+"""
+print(cost.value)
